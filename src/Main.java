51f9b59207f0bbca3754d8fada50c7bd034237ec
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-01-07 12:01:12 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 21000; // цена билета
+        int bonus = ticketPrice / 20; // расчет бонуса
+        System.out.println("Вам начислено " + bonus + " балла(ов)");
+
+    }
+}

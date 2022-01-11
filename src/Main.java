a5d3bb507ed3971ce3e32553062b813ec835485e
Aleksandr Shalagin <ssanchesko@gmail.com>
2022-01-11 15:53:56 +0500

    Upgrade

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 21000; // цена билета
-        int bonus = ticketPrice / 20; // расчет бонуса
+        BonusMilesService service = new BonusMilesService();
+        int ticketPrice = 20_000; // цена билета
+        int bonus = service.calculate (ticketPrice);
         System.out.println("Вам начислено " + bonus + " балла(ов)");
 
     }
 }
+
+// public class Main {
+//    public static void main(String[] args) {
+//        BonusMilesService service = new BonusMilesService();
+//        int price = 10_000;
+//        int miles = service.calculate(price);
+//        System.out.println(miles);
+//    }
+//}
